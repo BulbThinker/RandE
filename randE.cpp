1a014bc0d6a470a64cfc27455e4dbc95bba7d03b
BulbThinker <matheusgiovannidias@live.com>
2019-12-27 00:11:05 -0300

    Added LCG algorithm

--- randE.cpp
+++ randE.cpp
@@ -5,47 +5,64 @@
  *  randE.cpp
  */
 
-#include "randE.h"
+#include <randE.h>
 
-const int rand_core[10] PROGMEM = {
+#define RAND_CORE_SIZE 10
+
+const uint32_t rand_core[RAND_CORE_SIZE] PROGMEM = {
     0xf871887a,0x468f2917,0x9259fe14,0xd45c8b85,0x58345674,
     0x2dc48009,0x75b328a5,0x45c30745,0x10bd6c99,0xfb914f8c,
 };
 
-TrueRand::TrueRand(int analog_pin) {
-    pinMode(analog_pin, INPUT);
-    this->analog_pin = analog_pin;
-    this->last_v = rand_core[analogRead(analog_pin)%10];
+int ARand::next_int() {
+    return ((int)(this->getRandomDWORD() & 0x00007FFF));
 }
 
-uint32_t TrueRand::getRandomDWORD() {
-    uint32_t x = ((this->last_v ^ rand_core[x % 10]) & 0xFFFFF000)
-               | ((millis()&0xFF) << 4)
-               | (analogRead(this->analog_pin)&0xF);
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return (this->last_v = x);
-}
-
-int TrueRand::next_int() {
-    return ((int)this->getRandomDWORD());
-}
-
-int TrueRand::next_int(int min_v, int max_v) {
+int ARand::next_int(int min_v, int max_v) {
     return ((int)((max_v - min_v)*this->next_double() + min_v));
 }
 
-float TrueRand::next_float() {
+float ARand::next_float() {
     return ((float)this->getRandomDWORD()/4294967296.0f);
 }
 
-double TrueRand::next_double() {
+double ARand::next_double() {
     return ((double)this->getRandomDWORD()/4294967296.0);
 }
 
+double ARand::next_unit_interval() {
+	return ((double)this->getRandomDWORD()/4294967295.0);
+}
 
-XorShift::XorShift(uint32_t seed) {
+double ARand::next_normal(void) {
+	double u1 = this->next_unit_interval();
+	double u2 = this->next_unit_interval();
+	return (sqrt(-2.0 * log(u1)) * cos(6.28318530718*u2));
+}
+
+double ARand::next_normal(double mean, double std) {
+	return ((this->next_normal() * std) + mean);
+}
+
+/*=================================================================*/
+
+TrueRand::TrueRand(int analog_pin) {
+    pinMode(analog_pin, INPUT);
+    this->analog_pin = analog_pin;
+    this->last_v = rand_core[analogRead(analog_pin) % RAND_CORE_SIZE];
+}
+
+uint32_t TrueRand::getRandomDWORD() {
+    uint32_t x = ((this->last_v ^ rand_core[x % RAND_CORE_SIZE]) & 0xFFFFF000)
+               | ((millis()&0xFF) << 4)
+               | (analogRead(this->analog_pin)&0xF);
+    x = (((1103515245 * x) + 12345) & 0x7FFFFFFF);
+    return (this->last_v = x);
+}
+
+/*=================================================================*/
+
+XorShift::XorShift(uint32_t seed = 1) {
     this->seed = seed;
 }
 
@@ -57,19 +74,15 @@ uint32_t XorShift::getRandomDWORD() {
     return this->seed = x;
 }
 
-int XorShift::next_int() {
-    return ((int)this->getRandomDWORD());
+/*=================================================================*/
+
+LCG::LCG(uint32_t seed = 1) {
+	this->seed = seed;
 }
 
-int XorShift::next_int(int min_v, int max_v) {
-    return ((max_v - min_v)*this->next_double() + min_v);
+uint32_t LCG::getRandomDWORD() {
+	this->seed = (((1103515245 * this->seed) + 12345) & 0x7FFFFFFF);
+	return this->seed;
 }
 
-float XorShift::next_float() {
-    return ((float)this->getRandomDWORD()/4294967296.0f);
-}
-
-double XorShift::next_double() {
-    return ((double)this->getRandomDWORD()/4294967296.0);
-}
-
+/*=================================================================*/
--- randE.h
+++ randE.h
@@ -11,41 +11,45 @@
 #include <Arduino.h>
 
 
-class IRand {
+class ARand {
     private:
         virtual uint32_t getRandomDWORD(void) = 0;
     public:
-        virtual int next_int(void) = 0;
-        virtual int next_int(int min_v, int max_v) = 0;
-        virtual float next_float(void) = 0;
-        virtual double next_double(void) = 0;
+        virtual int next_int(void);
+        virtual int next_int(int min_v, int max_v);
+        virtual float next_float(void);
+        virtual double next_double(void);
+		virtual double next_unit_interval(void);
+		virtual double next_normal(void);
+		virtual double next_normal(double mean, double std);
 };
 
 
-class TrueRand : public IRand{
+class TrueRand : public ARand{
     private:
         int analog_pin;
         uint32_t last_v;
         uint32_t getRandomDWORD(void);
     public:
         TrueRand(int analog_pin);
-        int next_int(void);
-        int next_int(int min_v, int max_v);
-        float next_float(void);
-        double next_double(void);
 };
 
 
-class XorShift : public IRand {
+class XorShift : public ARand {
     private:
         uint32_t seed;
         uint32_t getRandomDWORD(void);
     public:
         XorShift(uint32_t seed);
-        int next_int(void);
-        int next_int(int min_v, int max_v);
-        float next_float(void);
-        double next_double(void);
+};
+
+class LCG : public ARand {
+	private:
+        uint32_t seed;
+        uint32_t getRandomDWORD(void);
+		uint32_t A, C, M;
+    public:
+        LCG(uint32_t seed);
 };
 
 #endif
